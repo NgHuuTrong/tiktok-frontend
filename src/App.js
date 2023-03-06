8b3416484101b9547050dc6a85fa55d3ca7e7b35
NgHuuTrong <trongnh2003@gmail.com>
2023-03-06 23:47:49 +0700

    Configure 'Router/Layout' for project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import GlobalStyles from './components/GlobalStyles';
+import { Fragment } from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Layout = route.layout ? route.layout : route.layout === null ? Fragment : DefaultLayout;
+            const Pages = route.component;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Pages />
+                  </Layout>
+                }
+              ></Route>
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
