42e3785ce213cbebdb5f04898e649043d120222c
NgHuuTrong <trongnh2003@gmail.com>
2023-04-11 10:01:00 +0700

    (Sidebar #3) Fetch API (from rapidAPI)

--- src/services/searchService.js
+++ src/services/searchService.js
@@ -1,16 +1,16 @@
-import * as httpRequest from '~/utils/httpRequest';
+// import * as httpRequest from '~/utils/httpRequest';
 
-export const search = async (keywords, count, cursor = '0') => {
-  const searchOptions = {
-    method: 'GET',
-    url: `${process.env.REACT_APP_BASE_URL}user/search`,
-    params: { keywords, count, cursor },
-    headers: {
-      'X-RapidAPI-Key': '4ad34b516cmsh9be8d0d889bb84cp1030f9jsn2446e27d5d3a',
-      'X-RapidAPI-Host': 'tiktok-video-no-watermark2.p.rapidapi.com',
-    },
-  };
+// export const search = async (keywords, count, cursor = '0') => {
+//   const searchOptions = {
+//     method: 'GET',
+//     url: `https://tiktok-video-no-watermark2.p.rapidapi.com/user/search`,
+//     params: { keywords, count, cursor },
+//     headers: {
+//       'X-RapidAPI-Key': '8b2882fc81msh436eeb150ff7158p1ab3bcjsndd6623c2580b',
+//       'X-RapidAPI-Host': 'tiktok-video-no-watermark2.p.rapidapi.com',
+//     },
+//   };
 
-  const data = await httpRequest.get(searchOptions);
-  return data;
-};
+//   const response = await httpRequest.get(searchOptions);
+//   return response;
+// };
---
+++ src/services/suggestService.js
@@ -0,0 +1,16 @@
+// import * as httpRequest from '~/utils/httpRequest';
+
+// export const getSuggest = async (id, cursor = '20') => {
+//   const suggestOptions = {
+//     method: 'GET',
+//     url: 'https://tiktok-all-in-one.p.rapidapi.com/user/suggest',
+//     params: { id, cursor },
+//     headers: {
+//       'X-RapidAPI-Key': '4ad34b516cmsh9be8d0d889bb84cp1030f9jsn2446e27d5d3a',
+//       'X-RapidAPI-Host': 'tiktok-all-in-one.p.rapidapi.com',
+//     },
+//   };
+//   const data = await httpRequest.get(suggestOptions);
+//   console.log(id, 'data:', data);
+//   return data;
+// };
